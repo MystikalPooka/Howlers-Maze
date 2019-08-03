d87fcacec110fc2ee5670e6a916eac817b510249
Joshua Lutz <MystikalPooka@gmail.com>
2019-08-02 17:35:17 -0700

    initial commit

---
+++ Assets/Move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Move : MonoBehaviour
+{
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(new Vector3(1, 1, 1));
+        Debug.Log(agent.nextPosition);
+    }
+}
